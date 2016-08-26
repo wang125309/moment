bb6b6c2f24577e604db024cf338a0c96f66d900d
wang125309 <wang_125309@foxmail.com>
2016-08-27 02:09:40 +0800

    css

--- static/js-modify/edit/index.js
+++ static/js-modify/edit/index.js
@@ -5,7 +5,27 @@ require("../../bower_components/swiper/dist/js/swiper.min.js");
 require("../../bower_components/angular/angular.min.js");
 require("../../bower_components/angular-sanitize/angular-sanitize.min.js");
 require("../../bower_components/angular-touch/angular-touch.min.js");
-require("./share.js");
+$(function(){
+	var shareJson = {
+		link:"http://jianfei.7cha.com/moment/",
+		imgUrl:"http://jianfei.7cha.com/moment/static/image/share-image.jpg",
+		title:"慎点！关八会长的朋友圈有毒……",
+		desc:"关八会长深夜放毒，明星“偷吃”亟待拯救"
+
+	};
+	function iniWxShare (d){
+		wx.onMenuShareAppMessage({title: d.title,desc: d.desc,link: d.link,imgUrl: d.logo,success: function (res) { } }); //好友
+		wx.onMenuShareTimeline({title: d.title+","+d.desc,desc: d.title,link: d.link,imgUrl: d.logo,success: function (res) { } });  //朋友圈
+	}
+	$.get("http://jianfei.7cha.com/data/index.php","",function(d){
+		wx.config({
+			appId: d.appId,timestamp: d.timestamp,nonceStr: d.nonceStr,signature: d.signature,
+			jsApiList: ['checkJsApi','onMenuShareTimeline','onMenuShareAppMessage']
+		});
+		wx.ready(function () {iniWxShare(shareJson)});
+		wx.error(function (res) { });
+	},"jsonp")
+});
 
 Ctrl = angular.module('app',['ngSanitize','ngTouch']).controller('Ctrl',['$sce','$scope',function($sce,$scope){
     var get_emoj = function(emoj) {
