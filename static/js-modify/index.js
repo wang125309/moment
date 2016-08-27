28714891331cdbe8f4dcd637af4bd16f6f974f06
wang125309 <wang_125309@foxmail.com>
2016-08-27 14:17:33 +0800

    xxx

--- static/js-modify/index.js
+++ static/js-modify/index.js
@@ -221,7 +221,7 @@ Ctrl = angular.module('app',['ngSanitize','ngTouch']).controller('Ctrl',['$sce',
                 already_cai = true;
                 caiInterval = setInterval(function () {
                     $scope.do_cai(false);
-                }, 300);
+                }, 1000);
             }
             else if (cai_cnt == 2) {
                 $($(".pic-area")[$(".pic-area").length - 2]).addClass("rotate-and-down");
@@ -367,6 +367,7 @@ Ctrl = angular.module('app',['ngSanitize','ngTouch']).controller('Ctrl',['$sce',
                         else {
                             $(".b"+(b_cnt-1)).css("display","none");
                             $(".b"+b_cnt).css("display","block");
+                            $(".black-area .find").css("bottom","0.5rem");
                             b_cnt ++;
                         }
 
@@ -385,6 +386,7 @@ Ctrl = angular.module('app',['ngSanitize','ngTouch']).controller('Ctrl',['$sce',
                         else {
                             $(".b"+(b_cnt-1)).css("display","none");
                             $(".b"+b_cnt).css("display","block");
+                            $(".black-area .find").css("bottom","0.7rem");
                             b_cnt ++;
                         }
 
@@ -459,6 +461,7 @@ Ctrl = angular.module('app',['ngSanitize','ngTouch']).controller('Ctrl',['$sce',
                         else {
                             $(".b"+(b_cnt-1)).css("display","none");
                             $(".b"+b_cnt).css("display","block");
+                            $(".black-area .find").css("bottom","0.8rem");
                             b_cnt ++;
                         }
 
